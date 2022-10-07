1d2fc4fa79d2e6c14294075506ee01fb606ae526
Your Name <you@example.com>
2022-10-07 12:33:17 +0530

    message

--- Stack/Balanced Paranthesis/Main.java
+++ Stack/Balanced Paranthesis/Main.java
@@ -1,5 +1,35 @@
-package Stack.Balanced Paranthesis;
+import java.util.*;
 
-public class Main {
-    
+class Main {
+    static boolean checkPair(char c1, char c2) {
+        if (c1 == '{' && c2 == '}' || c1 == '[' && c2 == ']' || c1 == '(' && c2 == ')')
+            return true;
+        else
+            return false;
+    }
+
+    public static Boolean checkBalanced(String s) {
+
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.isEmpty())
+                    return false;
+                char ch = stack.pop();
+                if (!checkPair(ch, s.charAt(i)))
+                    return false;
+            }
+        }
+        if (stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String args[]) {
+        String s = "{";
+        System.out.println(checkBalanced(s) ? "Balanced." : "Not Balanced");
+    }
 }
