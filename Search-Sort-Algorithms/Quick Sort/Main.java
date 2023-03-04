34e03de5a506331d13751faeb15ad2c14dc4a176
rinchentb <rinchentb26@gmail.com>
2023-03-04 20:59:12 +0530

    quick_sort

---
+++ Search-Sort-Algorithms/Quick Sort/Main.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+/*
+ *  Quick Sort Algorithm
+ *  -> Recursive -> Divide and Conquer
+ *  -> Time Complexities
+ *     Best Case - (nlogn)
+ *     Average Case - (nlogn)
+ *     Worst Case - (n^2)
+ */
+class Main {
+    static int partition(int[] arr, int l, int h) {
+        int start = l;
+        int end = h;
+        int pivot = arr[l];
+        int temp;
+        while (start < end) {
+            while (start <= h && arr[start] <= pivot)
+                start++;
+            while (end >= l && arr[end] > pivot)
+                end--;
+            if (start < end) {
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+            } else {
+                temp = arr[l];
+                arr[l] = arr[end];
+                arr[end] = temp;
+                break;
+            }
+        }
+
+        return end;
+    }
+
+    static void quick_sort(int[] arr, int l, int h) {
+        if (l < h) {
+            int pi = partition(arr, l, h);
+            quick_sort(arr, l, pi - 1);
+            quick_sort(arr, pi + 1, h);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 2, 1, 0, -1, -2, -3, -1000 };
+        System.out.print("Initial Array: ");
+        System.out.println(Arrays.toString(arr));
+        quick_sort(arr, 0, arr.length - 1);
+        System.out.print("Sorted Array: ");
+        System.out.println(Arrays.toString(arr));
+    }
+}
