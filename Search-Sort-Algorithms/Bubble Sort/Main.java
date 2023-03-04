a15b6747c76e50b35123b0ed19451c028824238d
rinchentb <rinchentb26@gmail.com>
2023-03-04 14:45:48 +0530

    sort

--- Search-Sort-Algorithms/Bubble Sort/Main.java
+++ Search-Sort-Algorithms/Bubble Sort/Main.java
@@ -1,34 +1,31 @@
+import java.util.Arrays;
+
 class Main {
-    static void bubble_sort(int arr[]) {
+
+    static void bubble_sort(int[] arr) {
         int n = arr.length;
-        int i, j, temp;
-        boolean swapped = false;
-        for (i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (j = 0; j < n - 1 - i; j++) {
+        boolean didSwap;
+        for (int i = 0; i < n - 1; i++) {
+            didSwap = false;
+            for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    temp = arr[j];
+                    int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-                    swapped = true;
+                    didSwap = true;
                 }
             }
-            if (swapped == false) {
+            if (didSwap == false)
                 break;
-            }
         }
     }
 
-    static void display(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + "\t");
-        }
-    }
-
-    public static void main(String args[]) {
-        int arr[] = { 6, 7, 8, 10, 9 };
+    public static void main(String[] args) {
+        int[] arr = { 123, 23, 13, 0, -2 };
+        System.out.print("Initial Array: ");
+        System.out.println(Arrays.toString(arr));
         bubble_sort(arr);
-        display(arr);
+        System.out.print("Sorted Array: ");
+        System.out.println(Arrays.toString(arr));
     }
 }
