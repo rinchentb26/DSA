a740ddf7adaee8ceccd55a77d7dbc1c7fed29cda
rinchentb <rinchentb26@gmail.com>
2023-03-08 14:48:44 +0530

    infixtoPost

--- Stack/InfixToPostfix/Main.java
+++ Stack/InfixToPostfix/Main.java
@@ -1,52 +1,93 @@
-import java.util.ArrayDeque;
+import java.util.Scanner;
+import java.util.Stack;
 
 class Main {
-    static boolean isOperand(char c) {
-        if (Character.isLetter(c))
-            return true;
-        else
-            return false;
+    static void convertToPostfix(String str) {
+        Stack<Character> s = new Stack<>();
+        String post_exp = "";
+        char ch;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            // if operand directly add to result
+            if (isOperand(ch)) {
+                post_exp += ch;
+            }
+            /*
+             * if operator, push if -> stack is empty
+             * -> precedence is greater than precedence of top item in stack
+             * if precedence is small
+             * -> pop and add to result until precedence of current operator is greater or
+             * stack is empty
+             * -> push operator to stack
+             * 
+             */
+
+            else if (isOperator(ch)) {
+                if (s.isEmpty() || getPrecedence(ch) > getPrecedence(s.peek()))
+                    s.push(ch);
+                else {
+                    while ((!s.isEmpty()) && (getPrecedence(ch) <= getPrecedence(s.peek()))) {
+                        post_exp += s.pop();
+                    }
+                    s.push(ch);
+                }
+            }
+            /*
+             * if it is opening bracket simply push to stack,
+             * if it is closing bracket pop all items from stack and add to result
+             * until closing brace is found
+             */
+            else {
+                if (ch == '(')
+                    s.push(ch);
+                else {
+                    char popped;
+                    while ((!s.isEmpty()) && ((popped = s.pop()) != '(')) {
+                        post_exp += popped;
+                    }
+                }
+            }
+        }
+        // after iterating through the expression, make sure stack is empty by popping
+        // all elements and add to res
+        while (!s.isEmpty()) {
+            post_exp += s.pop();
+        }
+        System.out.println("Converted Postfix Expression: " + post_exp);
+
     }
 
-    static int operatorPrecedence(char c) {
-        if (c == '^')
+    static boolean isOperand(char ch) {
+        if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9')
+            return true;
+        return false;
+    }
+
+    static boolean isOperator(char ch) {
+        if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^' || ch == '$')
+            return true;
+        return false;
+    }
+
+    public static int getPrecedence(char ch) {
+        if (ch == '^' || ch == '$')
             return 3;
-        else if (c == '*' || c == '/')
+        else if (ch == '*' || ch == '/')
             return 2;
-        else if (c == '+' || c == '-')
+        else if (ch == '+' || ch == '-')
             return 1;
         else
             return 0;
     }
 
-    static void infixToPostfix(String s) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (isOperand(s.charAt(i)))
-                System.out.print(s.charAt(i));
-            else if (s.charAt(i) == '(')
-                stack.push(s.charAt(i));
-            else if (s.charAt(i) == ')') {
-                while ((!stack.isEmpty()) && stack.peek() != '(') {
-                    System.out.print(stack.pop());
-                }
-                stack.pop();
-            } else if (stack.isEmpty() || operatorPrecedence(s.charAt(i)) > operatorPrecedence(stack.peek())) {
-                stack.push(s.charAt(i));
-            } else {
-                while ((!stack.isEmpty()) && operatorPrecedence(stack.peek()) >= operatorPrecedence(s.charAt(i))) {
-                    System.out.print(stack.pop());
-                }
-                stack.push(s.charAt(i));
-            }
-        }
-        while (!stack.isEmpty()) {
-            System.out.println(stack.pop());
-        }
-    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String expression;
+        System.out.print("Enter Infix Expression: ");
+        expression = sc.next();
+        System.out.println("Infix Expression: " + expression);
+        convertToPostfix(expression);
+        sc.close();
 
-    public static void main(String args[]) {
-        String expression = "a+b*(c^d-e)^(f+g*h)-i";
-        infixToPostfix(expression);
     }
 }
