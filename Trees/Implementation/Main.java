55c6ca690f6eb35eacace5c9c1bc78b50542c582
rinchentb <rinchentb26@gmail.com>
2023-03-09 20:58:50 +0530

    countNodes

--- Trees/Implementation/Main.java
+++ Trees/Implementation/Main.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 class Node {
     char data;
     Node left;
@@ -14,14 +16,17 @@ class Main {
         Node root = new Node('A');
         root.left = new Node('B');
         root.right = new Node('C');
-        root.left.left = new Node('D');
-        root.left.right = new Node('E');
+        // root.left.left = new Node('D');
+        // root.left.right = new Node('E');
         System.out.print("PREORDER TRAVERSAL: ");
         preorder(root);
         System.out.print("\nINORDER TRAVERSAL: ");
         inorder(root);
         System.out.print("\nPOSTORDER TRAVERSAL: ");
         postorder(root);
+        System.out.print("\nLEVEL ORDER TRAVERSAL: ");
+        levelOrder(root);
+        System.out.print("\nCOUNT OF NODES: " + countNodes(root));
     }
 
     public static void preorder(Node root) {
@@ -48,5 +53,31 @@ class Main {
             System.out.print(root.data + "\t");
         }
 
+    }
+
+    public static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        LinkedList<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node popped = q.remove();
+            System.out.print(popped.data + "\t");
+            if (popped.left != null)
+                q.add(popped.left);
+            if (popped.right != null)
+                q.add(popped.right);
+        }
+    }
+
+    static int countNodes(Node root) {
+        if (root != null) {
+            int x, y;
+            x = countNodes(root.left);
+            y = countNodes(root.right);
+            return x + y + 1;
+        }
+        return 0;
     }
 }
